b6df6d557b2afa4558d376b02d6dd1b379e86854
raimonade <Romka995@gmail.com>
2022-05-13 14:12:27 +0300

    are these included?

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@
 const withSvgr = require('@newhighsco/next-plugin-svgr');
 const nextConfig = {
 	reactStrictMode: true,
-	env: {
-		API_URL: 'http://localhost:3000',
-		SOCKET_URL: 'http://localhost:8080',
-	},
 };
 
 module.exports = withSvgr(nextConfig);
